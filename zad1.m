2f5ba680d5e0eeca4ad75416858ce7a88d62bd8a
Micha <mlk04@wp.pl>
2019-04-03 13:59:01 +0200

    origin master

--- zad1.m
+++ zad1.m
@@ -7,26 +7,31 @@ counter_down = 0;
 values = zeros(100,1);
 
 for prog = 0.01:0.01:1
-    for i = 1:1:y
-        for j = 1:1:x
-            value = I1(i,j);
-            if(value<prog) 
-                I(i,j,1) = 1;
-                I(i,j,2) = 0;
-                I(i,j,3) = 0;
-                counter_down=counter_down+1;
-            else
-                I(i,j,1) = 0;
-                I(i,j,2) = 1;
-                I(i,j,3) = 0;
-            end
-        end
-    end
-    values=[counter_down;values(1:end-1)];
+%     for i = 1:1:y
+%         for j = 1:1:x
+%             value = I1(i,j);
+%             if(value<prog) 
+%                 I(i,j,1) = 1;
+%                 I(i,j,2) = 0;
+%                 I(i,j,3) = 0;
+%                 counter_down=counter_down+1;
+%             else
+%                 I(i,j,1) = 0;
+%                 I(i,j,2) = 1;
+%                 I(i,j,3) = 0;
+%             end
+%         end
+%     end
+
+    I2 = I1>prog;
+    imshow(I2)
+    counter=sum(I2(:))
+    values=[counter;values(1:end-1)];
 end
 
 figure
-plot((0.01:0.01:1),values)
+plot((0.01:0.01:1),fliplr(values))
+%imshow(I2);
 
 % c = uicontrol(fig,'Style','pushbutton');
 % c.Callback = @plotButtonPushed;
--- zad2.m
+++ zad2.m
@@ -1,49 +1,65 @@
+global chosen_gray;
+
 I = imread('kotek.jpg');
 I = im2double(I);
-I = rgb2gray(I);
-a=imadjust(I,[0 0.7]);
-imshow(a);
+%a=imadjust(I,[0 0.7]);
+fig = figure(1);
+imshow(I);
+[chosen_region, coords] = imcrop(I);
+chosen_gray = rgb2gray(chosen_region);
+c = uicontrol('Parent',fig,'style','slider','units','normalized','position',[0,0,0.1,1.0]);
+c.Callback = @plotSlid;
+
+figure(2)
+tmp = imshow(chosen_gray);
+
+
    
-%       figure, imshow(K
-% c = uicontrol(fig,'Style','pushbutton');
-% c.Callback = @plotButtonPushed;
-% 
-%     function plotButtonPushed(src,event)
-%         bar(randn(1,5));
-%     end
 
 
-function out = imadjust1(varargin)
 
-%Parse inputs and initialize variables
-[img,imageType,lowIn,highIn,lowOut,highOut,gamma] = ...
-    parseInputs(varargin{:});
-
-validateLowHigh(lowIn,highIn,lowOut,highOut);
-gamma = validateGamma(gamma,imageType);
-
-if ~isfloat(img) && numel(img) > 65536
-    % integer data type image with more than 65536 elements
-    out = adjustWithLUT(img,lowIn,highIn,lowOut,highOut,gamma);
-
-else
-    classin = class(img);
-    classChanged = false;
-    if ~isa(img,'double')
-        classChanged = true;
-        img = im2double(img);
-    end
-
-    if strcmp(imageType, 'intensity')
-        out = adjustGrayscaleImage(img,lowIn,highIn,lowOut,highOut,gamma);
-    elseif strcmp(imageType, 'indexed')
-        out = adjustColormap(img,lowIn,highIn,lowOut,highOut,gamma);
-    else
-        out = adjustTruecolorImage(img,lowIn,highIn,lowOut,highOut,gamma);
-    end
+function plotSlid(src,event)
+    global chosen_gray;
+    a = get(src,'value')
+    kontrast = max(max(chosen_gray)) - min(min(chosen_gray));
+    tmp_gray=chosen_gray*a;
+    figure(2);
+    imshow(tmp_gray);
     
-    if classChanged
-        out = images.internal.changeClass(classin,out);
-    end
 end
-end
+
+
+% function out = imadjust1(varargin)
+% 
+% %Parse inputs and initialize variables
+% [img,imageType,lowIn,highIn,lowOut,highOut,gamma] = ...
+%     parseInputs(varargin{:});
+% 
+% validateLowHigh(lowIn,highIn,lowOut,highOut);
+% gamma = validateGamma(gamma,imageType);
+% 
+% if ~isfloat(img) && numel(img) > 65536
+%     % integer data type image with more than 65536 elements
+%     out = adjustWithLUT(img,lowIn,highIn,lowOut,highOut,gamma);
+% 
+% else
+%     classin = class(img);
+%     classChanged = false;
+%     if ~isa(img,'double')
+%         classChanged = true;
+%         img = im2double(img);
+%     end
+% 
+%     if strcmp(imageType, 'intensity')
+%         out = adjustGrayscaleImage(img,lowIn,highIn,lowOut,highOut,gamma);
+%     elseif strcmp(imageType, 'indexed')
+%         out = adjustColormap(img,lowIn,highIn,lowOut,highOut,gamma);
+%     else
+%         out = adjustTruecolorImage(img,lowIn,highIn,lowOut,highOut,gamma);
+%     end
+%     
+%     if classChanged
+%         out = images.internal.changeClass(classin,out);
+%     end
+% end
+% end
